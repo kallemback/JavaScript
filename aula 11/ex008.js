ba9d55c2e26efd7c18c2086817713527b244cdae
Alek Marcondes <89283892+kallemback@users.noreply.github.com>
2023-06-11 18:26:59 -0300

    Initial commit

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.1
+console.log(`A velocidade de seu carro é ${vel}Km/h`)
+if (vel > 60){  //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Diriga sempre com o cinto de segurança!!!`)
---
+++ aula10/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70.5
+console.log (`A velocidade do seu carro é de ${vel} km/h`)
+if (vel > 70){
+    console.log('Você ultrapassou o limite de velocidade. Você foi MULTADO!')
+}
+console.log ('Dirija sempre com atenção!')
+
