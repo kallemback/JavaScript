be7412dae8e905a0efffe926c0a4bf45086311e9
Alek Marcondes <89283892+kallemback@users.noreply.github.com>
2023-06-12 18:54:43 -0300

    update

---
+++ aula14ex/ambiente.js
@@ -0,0 +1,14 @@
+console.log('Vai começar...')
+for (var c=1; c <= 7; c++ ){
+    console.log(c)
+}
+console.log('FIM')
+
+//Mesma coisa que 
+
+/*
+var c = 1
+while(c <= 5){
+    c++
+    console.log(c)
+} */
