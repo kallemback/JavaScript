ceee4fa440a1df9d6a2f900c6401bedf4f5dbcd4
Alek Marcondes <89283892+kallemback@users.noreply.github.com>
2023-06-13 15:49:15 -0300

    analisador concluido

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if (n%2 == 0 ){
+        return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+}
+var res = parimpar(67)
+    //console.log(res)
+
+    console.log(parimpar(32))
